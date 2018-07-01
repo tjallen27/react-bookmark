ffca13f63b76d7fbe2aaf1824d6c28ee4521f834
tjallen27 <tjallen91@hotmail.co.uk>
2018-07-01 18:06:57 +0100

    Styling updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import ListItems from './Components/ListItems';
 import './App.css';
+const appStyles = {
+  fontWeight: '300',
+  color: '#333'
+}
+const headerStyles = {
+  marginTop: '0',
+  color: 'white',
+  background: 'rgba(100, 100, 245, 1)'
+};
+const h1Styles = {
+  marginTop: '0',
+  padding: '20px 0'
+}
+const formStyles = {
+  margin: '10px',
+  padding: '10px',
+  outline: 'none',
+  border: 'none',
+  borderRadius: '5px'
+}
+
 
 export default class App extends Component {
   constructor(props){
@@ -40,7 +61,7 @@ export default class App extends Component {
   }
 
   removeItem(index) {
-    console.log(index)
+    console.log('remove item')
     var items = this.state.items;
     items.splice(index, 1)
     this.setState({items})
@@ -48,15 +69,16 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feast It: Bookmark List</h1>
+      <div style={appStyles} className="App">
+        <header style={headerStyles} className="App-header">
+          <h1 style={h1Styles} className="App-title">Feast It: Bookmark List</h1>
         </header>
 
-        <form onSubmit={this.onSubmit}>
-          <span>https://</span><input value={this.state.value} onChange={this.onChange} />
-          <button>Save</button>
+        <form style={formStyles} onSubmit={this.onSubmit}>
+          <span>https:// </span><input value={this.state.value} onChange={this.onChange} />
+          <button style={formStyles}>Save</button>
         </form>
+
         <ListItems items={this.state.items} url={this.state.url} removeItem={this.removeItem}/>
       </div>
     );
--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+const formStyles = {
+  padding: '10px',
+  outline: 'none',
+  border: 'solid 1px #555',
+  borderRadius: '5px'
+}
+const listStyles = {
+  listStyle: 'none'
+}
+
 const List = props => (
   <ul>
     {
-      props.items.map((item, index) => <li key={index}><a href={props.url} target="_blank">{item}</a> <button type="button" className="close" onClick={props.removeItem}>X</button></li>)
+      props.items.map((item, index) =>
+      <li
+      key={index}
+      style={listStyles}
+      >
+        <a href={props.url} target="_blank">{item}</a>
+        <button style={formStyles} type="button" className="close" onClick={props.removeItem}>x</button>
+      </li>)
     }
   </ul>
 );
